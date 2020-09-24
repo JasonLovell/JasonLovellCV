9c6614e65745cf9301346636bc2827d9fbd71655
Jason Lovell <lovellphotography@yahoo.co.uk>
2020-09-24 21:38:55 +0100

    Updates

---
+++ precache-manifest.a6c4be71e31d31f46cda56a8cda8f3ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c1cb8b0acc2bb5297ac7a3caf30bd8b",
+    "url": "/JasonLovellCV/index.html"
+  },
+  {
+    "revision": "b566fbab43529f64d920",
+    "url": "/JasonLovellCV/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "2dd7df76bd079bebc450",
+    "url": "/JasonLovellCV/static/css/main.5cf5d1b9.chunk.css"
+  },
+  {
+    "revision": "b566fbab43529f64d920",
+    "url": "/JasonLovellCV/static/js/2.a9c25fc5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/JasonLovellCV/static/js/2.a9c25fc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dd7df76bd079bebc450",
+    "url": "/JasonLovellCV/static/js/main.4990016a.chunk.js"
+  },
+  {
+    "revision": "d78b08945a46ae54c9ac",
+    "url": "/JasonLovellCV/static/js/runtime-main.036a8d26.js"
+  }
+]);
